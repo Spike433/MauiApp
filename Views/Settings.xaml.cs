83a48ea4a7326147ea3bd21e21263271e7cb5d51
Spike433 <juhacar116@gmail.com>
2023-10-29 23:08:46 +0100

    fix(); add qr generator

--- Views/Settings.xaml.cs
+++ Views/Settings.xaml.cs
@@ -1,4 +1,6 @@
 using AirApp.Models;
+using Controls.UserDialogs.Maui;
+using System.Net;
 
 namespace AirApp.Views;
 
@@ -15,10 +17,49 @@ public partial class Settings : ContentPage
         Temp.SortOnHumidity = sortValue;
     }
 
-    private void OnApplyFiltersClicked(object sender, EventArgs e)
+    private async void OnApplyFiltersClicked(object sender, EventArgs e)
     {
-        Temp.Start = startDatePicker.Date;
-        Temp.End = endDatePicker.Date;
+        DateTime startTDate = startDatePicker.Date + startTimePicker.Time;
+        DateTime endTDate = endDatePicker.Date + endTimePicker.Time;
 
+        var startUtc = startTDate.ToUniversalTime();
+        var endUtc = endTDate.ToUniversalTime();
+
+        // Combine the date and time to create a single DateTime
+
+
+        string BASE_URL = "https://losy-backend-dev-b1-plan.azurewebsites.net/api/";
+        var result = string.Format($"{BASE_URL}/QrCode/1?lockerName=3" +
+            $"&reservationStart={startUtc.ToString("yyyy-MM-ddTHH:mm:ss.fffZ")}" +
+            $"&reservationEnd={endUtc.ToString("yyyy-MM-ddTHH:mm:ss.fffZ")}" +
+            $"&userId=0", string.Empty);
+
+        Uri uri = new Uri(result);
+        var client = new HttpClient();
+
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync(result);
+            if (response.IsSuccessStatusCode)
+            {
+                Stream imageStream = await response.Content.ReadAsStreamAsync();
+
+                var imageSource = ImageSource.FromStream(() => imageStream);
+                qr.Source = imageSource;
+            }
+            else if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                string errorMessage = await response.Content.ReadAsStringAsync();
+                await Console.Out.WriteLineAsync(errorMessage);
+
+                // Display the error message to the user.
+                UserDialogs.Instance.Alert(errorMessage);
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
